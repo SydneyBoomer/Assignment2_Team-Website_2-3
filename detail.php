e206a069deb0673d711ce9ee580e0adaeaa81edb
SydneyBoomer <boomers1@mymail.nku.edu>
2026-09-23 21:24:09 -0400

    added echo statement to replace html statement in detail.php

--- detail.php
+++ detail.php
@@ -30,7 +30,7 @@
 <body>
     <article class="resume-wrapper text-center position-relative">
 		<?php /* Only the following line changed from the file in the previous assignment */ ?>
-		<div class="mb-4"><a href="index.php" class="btn btn-primary">Back to index</a></div>
+		<div class="mb-4"><a href="index.php" class="btn btn-primary"><?php echo "Back to index"?></a></div>
 	    <div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
 		    <header class="resume-header pt-4 pt-md-0">
 			    <div class="row">
